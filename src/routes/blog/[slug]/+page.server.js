b49f94a54bc8475de22713aeedc62d3e50834fdc
Rana Mahad Ahmer <ranamahadahmer@outlook.com>
2024-06-30 19:04:03 +0500

    -

--- src/routes/blog/[slug]/+page.server.js
+++ src/routes/blog/[slug]/+page.server.js
@@ -1,6 +1,17 @@
-export async function load({params}){
-    
-    return {
-        content:`Hello ${params.slug}`
-    }
+import {createClient} from '@vercel/postgres';
+
+async function queryNames() {
+  const client = createClient();
+  await client.connect();
+  try {
+      return await client.sql`SELECT * FROM NAMES;`
+  } finally {
+    await client.end();
+  }
+}
+
+export async function load({locals }) {
+  return {
+    names: await queryNames()
+  }
 }
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,6 +1,11 @@
-<script>
-    export let data
+<script lang="ts">
+    export let data;
 </script>
 
+<div>
+  {#each data.names as { name }}
+    <div class="font-semibold">{name}</div>
+  {/each}
+</div>
 
-<p class="font-bold text-center">{data.content}</p>
+<!--<p class="font-bold text-center">{data.content}</p>-->
